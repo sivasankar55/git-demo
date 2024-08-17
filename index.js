42fcc8dbc78da33b89ab6d94b1b1e1d6b5734b8a
sivasankar55 <sivasankar8875@gmil.com>
2024-08-17 13:25:57 +0530

    add new feature

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride= require("method-override");
+const ExpressError = require("./ExpressError");
+
+
+main().then(()=>{console.log("connection successful")})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
+}
 
 
 app.set("views",path.join(__dirname,"views"));
@@ -13,12 +22,6 @@ app.use(express.urlencoded({extended:true}));
 app.use (methodOverride("_method"));
 
 
-main().then(()=>{console.log("connection successful")})
-.catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
-}
 
 //index Route
 
@@ -30,11 +33,13 @@ app.get("/chats",async(req,res)=>{
 
 // new route
 app.get("/chats/new",(req,res)=>{
+  // throw new ExpressError(404,"page not found");
 res.render("new.ejs");
 });
 
 //create route
-app.post("/chats",(req,res)=>{
+app.post("/chats",async(req,res)=>{
+ try{
   let{from,to ,msg}= req.body;
   let newChat = new Chat({
     from:from,
@@ -48,7 +53,11 @@ app.post("/chats",(req,res)=>{
   .catch(err=>{
     console.log(err);
   });
+  await newChat.save();
   res.redirect("/chats");
+ }catch(err){
+  next(err);
+ }
 });
 
 
@@ -63,11 +72,25 @@ chat1.save().then((res)=>{
   console.log(res);
 });
 
+//new show route
+app.get("/chats/:id",async(req,res,next)=>{
+ let{id}=req.params;
+ let chat = await Chat.findById(id);
+ if(!chat){
+ next ( new ExpressError(404,"chat not found"));
+ }
+ res.render("edit.ejs",{chat});
+});
+
 //edit route
 app.get("/chats/:id/edit",async (req,res)=>{
+try{
   let {id}= req.params;
   let chat=await Chat.findById(id);
   res.render("edit.ejs", {chat});
+}catch(err){
+  console.log(err);
+}
 });
 
 //update route
@@ -95,6 +118,13 @@ app.delete("/chats/:id", async(req,res)=>{
 app.get("/", (req,res)=>{
     res.send("root is working");
 });
+
+//error handling middleware
+app.use((err,req,res,next)=>{
+let {status=500,message="Some Error Occurred"}=err;
+res.status(status).send(message);
+});
+
 
 app.listen(8080, ()=>{
     console.log("server is listening on port 8080");
--- models/chat.js
+++ models/chat.js
@@ -4,19 +4,19 @@ const mongoose = require("mongoose");
 const chatSchema = new mongoose.Schema({
     from:{
         type:String,
-        required:true
+        required:true,
     },
     to:{
         type:String,
-        required:true
+        required:true,
     },
     msg:{
         type:String,
-        maxLength:50
+        maxLength:50,
     },
     created_at:{
         type:Date,
-        require:true
+        require:true,
     },
 });
 
